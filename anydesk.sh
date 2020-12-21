7504fa3761633e741f2eddd077b86849373e1806
cucafresca <50235839+cucafresca@users.noreply.github.com>
2020-12-21 18:28:08 -0300

    Add files via upload
    anydesk

---
+++ anydesk.sh
@@ -0,0 +1,12 @@
+#bin bash
+
+wget https://download.anydesk.com/linux/anydesk_6.0.1-1_amd64.deb?_ga=2.146521588.1939341847.1608575593-798848712.1606659950
+
+mv anydesk_6.0.1-1_amd64.deb?_ga=2.146521588.1939341847.1608575593-798848712.1606659950 anydesk_6.0.1-1_amd64.deb
+
+apt install ./anydesk_6.0.1-1_amd64.deb
+
+systemctl disable anydesk
+
+
+
