1a4bf8e2f522072877000c723f37d9664820b1b5
cucafresca <50235839+cucafresca@users.noreply.github.com>
2021-05-17 11:42:13 -0300

    Update anydesk.sh

--- anydesk.sh
+++ anydesk.sh
@@ -4,7 +4,7 @@ wget https://download.anydesk.com/linux/anydesk_6.0.1-1_amd64.deb?_ga=2.14652158
 
 mv anydesk_6.0.1-1_amd64.deb?_ga=2.146521588.1939341847.1608575593-798848712.1606659950 anydesk_6.0.1-1_amd64.deb
 
-apt install ./anydesk_6.0.1-1_amd64.deb
+apt install ./anydesk_6.0.1-1_amd64.deb -y
 
 systemctl disable anydesk
 
